fa28d573cb8d735e5b54cfa44827aeef16133c89
Mariya Buzhava <bujava.masha@gmail.com>
2024-11-20 23:29:34 +0200

    step 5-6

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import { env } from './utils/env.js';
+import { getAllContacts, getContactById } from './services/contacts.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -23,6 +24,47 @@ export const setupServer = () => {
     res.json({
       message: 'Hello server!',
     });
+  });
+
+  app.get('/contacts', async (req, res) => {
+    try {
+      const contacts = await getAllContacts();
+
+      res.status(200).json({
+        status: 200,
+        message: 'Successfully found contacts!',
+        data: contacts,
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: 'Internal server error',
+        error: error.message,
+      });
+    }
+  });
+
+  app.get('/contacts/:contactId', async (req, res) => {
+    try {
+      const { contactId } = req.params;
+      const contact = await getContactById(contactId);
+
+      if (!contact) {
+        return res.status(404).json({
+          message: 'Contact not found',
+        });
+      }
+
+      res.status(200).json({
+        status: 200,
+        message: `Successfully found contact with id ${contactId}!`,
+        data: contact,
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: 'Internal server error',
+        error: error.message,
+      });
+    }
   });
 
   app.use('*', (req, res, next) => {
