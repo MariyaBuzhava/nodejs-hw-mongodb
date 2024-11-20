6ab63939680027d6cd27f46ec986876010ff8bdd
Mariya Buzhava <bujava.masha@gmail.com>
2024-11-20 22:57:34 +0200

    step 3

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import { initMongoConnection } from './db/initMongoConnection.js';
 import { setupServer } from './server.js';
+const bootstrap = async () => {
+  await initMongoConnection();
+  setupServer();
+};
 
-setupServer();
+bootstrap();
